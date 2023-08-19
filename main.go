aca6e48c4493ff72130cdfd350fea7b900cb527b
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-08-19 09:48:14 +0530

    Accept search string from user

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
-    "github.com/anirbandey1/ryuu/repos"
+	"flag"
+	"fmt"
+
+	"github.com/anirbandey1/ryuu/repos"
 )
 
+
 func main()  {
+    
 
+    flag.Parse()
 
-    repos.SearchAppimage("chrome")
+    args:= flag.Args()
+
+    var firstNonFlagArg string
+
+    if len(args) > 0 {
+        firstNonFlagArg = args[0]
+
+    }else {
+        fmt.Println("No non-flag argument provided")
+    }
+
+    switch firstNonFlagArg {
+        case "search":
+            fmt.Println("Search")
+            if len(args) >= 2{
+                fmt.Printf("Search : %s \n",args[1])
+                repos.SearchAppimage(args[1])
+            }else {
+                fmt.Println("Please provide the name of the app you are searching for")
+            }
+        case "install":
+            fmt.Println("Install")
+        default:
+            fmt.Println("Please provide a valid non-flag argument")
+
+    }
+
 
 
     
